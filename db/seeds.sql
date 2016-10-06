d8782b935d85b9a6a465ebeae1ed0f9cda0a96ca
SpiderByt3s <SpiderByt3s@Legendary-MacBook-Pro.local>
2016-10-06 13:40:33 -0700

    do it please

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Double-Double');
+INSERT INTO burgers (burger_name) VALUES ('Western Bacon Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Chicken Sandwich');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
